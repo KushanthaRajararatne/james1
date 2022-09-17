c2a4d9800aeff8498f6d3a9fde0afe5a010b29f2
K.R.Rajaratne <ranganathrajaratne@gmail.com>
2022-09-17 11:08:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,10 @@
-
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpSession;
 
 import java.util.*;
 
