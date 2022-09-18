cbd76c351a73f56ad84fb62d4c0da8c2935f5ea2
K.R.Rajaratne <ranganathrajaratne@gmail.com>
2022-09-18 13:35:25 +0530

    Create Dockerfile.f

---
+++ Dockerfile.f
@@ -0,0 +1,9 @@
+FROM nginx:stable
+RUN rm /etc/nginx/conf.d/*
+COPY nginx.conf /etc/nginx/nginx.conf
+RUN groupadd -r www-data && \
+useradd --no-log-init -r -g www-data www-data \
+touch /var/run/nginx.pid && \
+chown -R www-data:www-data /var/run/nginx.pid && \
+chown -R www-data:www-data /var/cache/nginx
+USER www-data
